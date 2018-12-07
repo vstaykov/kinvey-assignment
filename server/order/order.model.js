944f0dab5d52d8e80ca4f5e8f5016b9cfee8ce89
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-07 17:25:48 +0200

    Improve order schema

--- server/order/order.model.js
+++ server/order/order.model.js
@@ -2,17 +2,36 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+const orderedProductsValidator = function(products) {
+  return products && products.length > 0;
+};
+
+const orderedProductsValidationError = function(props) {
+  return `${props.path} array must contain at least 1 product`;
+};
+
 const orderSchema = new Schema({
   orderedOn: Date,
   ammount: Number,
   products: {
     type: [
       {
-        type: Schema.ObjectId,
-        ref: "Product"
+        quantity: {
+          type: Number,
+          min: 1,
+          default: 1
+        },
+        product: {
+          type: Schema.ObjectId,
+          ref: "Product"
+        }
       }
     ],
     required: true,
+    validate: {
+      validator: orderedProductsValidator,
+      msg: orderedProductsValidationError
+    },
     default: undefined
   },
   shippingPoint: {
@@ -24,7 +43,7 @@ const orderSchema = new Schema({
 orderSchema.pre("save", function(next) {
   this.orderedOn = this._id.getTimestamp();
   this.ammount = this.products.reduce(
-    (accumulator, product) => accumulator + product.price,
+    (accumulator, item) => accumulator + item.quantity * item.product.price,
     0
   );
 
