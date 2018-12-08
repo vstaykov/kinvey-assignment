246f09503b7ac73f8d9012f4ade227a3a5244113
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-08 14:48:29 +0200

    Add products API

---
+++ server/product/product.controller.js
@@ -0,0 +1,49 @@
+const Product = require("./product.model");
+
+const productProjection = "name image price quantityInStock";
+
+const buildProductsQuery = filters => {
+  const { keywords, category, minPrice, maxPrice } = filters;
+  const query = Product.find();
+
+  if (keywords) {
+    query.byKeywords(keywords);
+  }
+
+  if (category) {
+    query.byCategory(category);
+  }
+
+  if (minPrice) {
+    query.byMinPrice(minPrice);
+  }
+
+  if (maxPrice) {
+    query.byMaxPrice(maxPrice);
+  }
+
+  query.select(productProjection);
+
+  return query;
+};
+
+const getAllProducts = async () => {
+  const products = await Product.find({}, productProjection);
+
+  return products;
+};
+
+const getProducts = async filters => {
+  const query = buildProductsQuery(filters);
+
+  const products = await query.exec();
+
+  return products;
+};
+
+const getProduct = async id => {
+  const product = await Product.findById(id);
+  return product;
+};
+
+module.exports = { getAllProducts, getProducts, getProduct };
---
+++ server/product/product.route.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const productsController = require("./product.controller");
+const paramsValidator = require("./../utils/paramsValidator");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const { query } = req;
+  let products = [];
+
+  if (query) {
+    products = await productsController.getProducts(query);
+  } else {
+    products = await productsController.getAllProducts();
+  }
+
+  res.json(products);
+});
+
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (paramsValidator.checkIsValidId(id)) {
+    const product = await productsController.getProduct(id);
+    res.json(product);
+  } else {
+    res.status(400).send(`Invalid product ID: ${id}`);
+  }
+});
+
+module.exports = router;
