7a64a764d05195668e57fad13ba323f7a040bb09
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-09 00:06:27 +0200

    Add dbconfig.js changes

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,6 +1,21 @@
-const host = process.env.DB_HOST || "mongodb://localhost";
-const port = process.env.DB_PORT || 27017;
-const name = process.env.DB_NAME || "testdb";
-const url = `${host}:${port}/${name}`;
+const primaryHost = process.env.DB_PRIMARY_HOST || "localhost";
+const primaryPort = process.env.DB_PRIMARY_PORT || 27017;
+const secondaryHost = process.env.DB_SECONDARY_HOST || "localhost";
+const secondaryPort = process.env.DB_SECONDARY_PORT || 27018;
+const arbiterHost = process.env.DB_ARBITER_HOST || "localhost";
+const arbiterPort = process.env.DB_ARBITER_PORT || 27019;
+const dbName = process.env.DB_NAME || "productsCatalog";
+const dbProtocol = process.env.DB_PROTOCOL || "mongodb";
+const replicaName = process.env.DB_REPLICA_NAME || "productsCatalogRepl";
+const primaryUrl = `${primaryHost}:${primaryPort}`;
+const secondaryUrl = `${secondaryHost}:${secondaryPort}`;
+const arbiterUrl = `${arbiterHost}:${arbiterPort}`;
 
-module.exports = { host, port, name, url };
+module.exports = {
+  primaryUrl,
+  secondaryUrl,
+  arbiterUrl,
+  dbName,
+  dbProtocol,
+  replicaName
+};
