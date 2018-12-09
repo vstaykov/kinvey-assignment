9b56d31c5cd505bc06a4ab3fe3f7f2ff92a567cc
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-09 16:25:56 +0200

    Rename product.category.js to product-category.js

--- server/product/product.model.js
+++ server/product/product.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const categories = require("./product.category");
+const categories = require("./product-category");
 
 const { Schema } = mongoose;
 const urlRegExp = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/gi;
