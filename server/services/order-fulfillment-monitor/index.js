1a0af60334e6a159a42af44c9f8e73cf9936f2d6
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-09 20:08:11 +0200

    Refactor fulfilled orders monitor service

--- server/services/order-fulfillment-monitor/index.js
+++ server/services/order-fulfillment-monitor/index.js
@@ -2,23 +2,26 @@ const redis = require("./../../utils/redis");
 
 const KEY = "ordersFulfilled";
 const client = redis.createClient();
-const subscriber = redis.createClient()
+const subscriber = redis.createClient();
 
 const safeExit = async () => {
-    await subscriber.unsubscribeAsync();
-    await subscriber.quitAsync();
-    await client.quitAsync();
+  await subscriber.unsubscribeAsync();
+  await subscriber.quitAsync();
+  await client.quitAsync();
 };
 
 const getFulfilledOrder = async () => {
-  const orderJson = await client.rpopAsync(KEY)
+  const orderJson = await client.rpopAsync(KEY);
   const order = JSON.parse(orderJson);
 
   return order;
 };
 
 subscriber.on("connect", () => {
-  subscriber.subscribe(`__keyspace@0__:${KEY}`, err => console.log("Subscribed"));
+  subscriber.subscribe(`__keyspace@0__:${KEY}`, err => {
+    const message = err || "Successfully subscribed to keyspace";
+    console.log(message);
+  });
 });
 
 subscriber.on("message", async (channel, message) => {
