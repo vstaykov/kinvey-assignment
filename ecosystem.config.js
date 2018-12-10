d8b50db3e2c7dd9b4c615ca1d943b55cd31e31ee
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-10 15:00:51 +0200

    Implement process management and load balancing

---
+++ ecosystem.config.js
@@ -0,0 +1,42 @@
+const config = {
+  apps: [
+    {
+      name: "api",
+      script: "./server/services/api/index.js",
+      output: "",
+      error: "",
+      instances: 2,
+      env: {
+        NODE_ENV: "production",
+        DB_PRIMARY_URL: "",
+        DB_SECONDARY_URL: "",
+        DB_ARBITER_URL: "",
+        DB_NAME: "",
+        DB_PROTOCOL: "",
+        DB_REPLICA_NAME: "",
+        API_PORT: "",
+        API_LOG_PATH: "",
+        API_LOG_NAME: "",
+        REDIS_HOST: "",
+        REDIS_PORT: ""
+      }
+    },
+    {
+      name: "order-fulfillment-monitor",
+      script: "./server/services/order-fulfillment-monitor/index.js",
+      output: "",
+      error: "",
+      instances: 2,
+      env: {
+        NODE_ENV: "production",
+        REDIS_HOST: "",
+        REDIS_PORT: "",
+        KINVEY_USER: "",
+        KINVEY_SECRET: "",
+        KINVEY_APP: ""
+      }
+    }
+  ]
+};
+
+module.exports = config;
