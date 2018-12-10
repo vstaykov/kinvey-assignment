22b3a1a4332247e7be5549b03314126e7ea4efab
Ventsislav Staykov <vencislav.staykov@gmail.com>
2018-12-10 13:08:16 +0200

    Remove unnecessary process env variables

--- server/config/db-config.js
+++ server/config/db-config.js
@@ -1,15 +1,9 @@
-const primaryHost = process.env.DB_PRIMARY_HOST || "localhost";
-const primaryPort = process.env.DB_PRIMARY_PORT || 27017;
-const secondaryHost = process.env.DB_SECONDARY_HOST || "localhost";
-const secondaryPort = process.env.DB_SECONDARY_PORT || 27018;
-const arbiterHost = process.env.DB_ARBITER_HOST || "localhost";
-const arbiterPort = process.env.DB_ARBITER_PORT || 27019;
+const primaryUrl = process.env.DB_PRIMARY_URL || "localhost:27017";
+const secondaryUrl = process.env.DB_SECONDARY_URL || "localhost:27018";
+const arbiterUrl = process.env.DB_ARBITER_URL || "localhost:27019";
 const dbName = process.env.DB_NAME || "productsCatalog";
 const dbProtocol = process.env.DB_PROTOCOL || "mongodb";
 const replicaName = process.env.DB_REPLICA_NAME || "productsCatalogRepl";
-const primaryUrl = `${primaryHost}:${primaryPort}`;
-const secondaryUrl = `${secondaryHost}:${secondaryPort}`;
-const arbiterUrl = `${arbiterHost}:${arbiterPort}`;
 
 module.exports = {
   primaryUrl,
